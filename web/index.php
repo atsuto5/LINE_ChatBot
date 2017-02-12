0cc73158d7607dc3756557d733c240c5dbd7baf0
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-02-12 19:45:09 +0900

    tes

--- web/index.php
+++ web/index.php
@@ -27,12 +27,11 @@ $app->get('/', function() use($app) {
 });
 
 $app->post('/callback', function (Request $request) use ($app) {
-    $client = new GuzzleHttp\Client();
 
     $body = json_decode($request->getContent(), true);
     error_log($request->getContent());
 
-    $accessToken = "xXNBySf4S9RO1vIkD1RfCjKPn1nA+UCc9fybgJXhixe8k4ZOFrP7kTaG7ADNhTaglrHx48RtsZ+s7lSoS8/tTLOVV7bxQO5jQae+ohgmRBaonHf5gUTkmUVRQm8TZKAj8PnOnn6vfwqcmJvGbszWiwdB04t89/1O/w1cDnyilFU=";
+    $accessToken = getenv("LINE_ACCESS_TOKEN");
 
     $replyToken = $body["events"][0]["replyToken"];
     $text = $body["events"][0]["message"]["text"];
@@ -61,45 +60,10 @@ $app->post('/callback', function (Request $request) use ($app) {
     ));
     curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, TRUE);
     curl_setopt($ch, CURLOPT_PROXYPORT, '80');
-    curl_setopt($ch, CURLOPT_PROXY, 'http://fixie:hMiDdbvvZHgMotg@velodrome.usefixie.com');
+    curl_setopt($ch, CURLOPT_PROXY, getenv("FIXIE_URL_ONLY"));
     $result = curl_exec($ch);
+    error_log(json_encode($result));
     curl_close($ch);
-
-
-
-
-
-//    foreach ($body['events'] as $msg) {
-//        error_log(json_encode($msg));
-//
-//        $resContent = $msg['message'];
-//        $resContent['text'] = 'ｶﾞｯ';
-//
-//        $requestOptions = [
-//            'body' => json_encode([
-//                'to' => [$msg],
-//                'toChannel' => 1383378250, # Fixed value
-//                'eventType' => '138311608800106203', # Fixed value
-//                'content' => $resContent,
-//            ]),
-//            'headers' => [
-//                'Content-Type' => 'application/json; charset=UTF-8',
-//                'X-Line-ChannelID' => getenv("LINE_CHANNEL_ID"),
-//                'X-Line-ChannelSecret' => getenv("LINE_CHANNEL_SECRET"),
-//                'X-Line-Trusted-User-With-ACL' => getenv("LINE_CHANNEL_MID")
-//            ],
-//            'proxy' => [
-//                'https' => getenv("FIXIE_URL"),
-//            ],
-//        ];
-//
-//        try {
-//            $client->post('https://trialbot-api.line.me/v1/events', $requestOptions);
-//
-//        } catch (Exception $e) {
-//            error_log($e->getMessage());
-//        }
-//    }
 
     return 'OK';
 });
