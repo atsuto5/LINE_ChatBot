4d213c25e7e00f8fa6a2f3315966a84cb15ed368
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-02-12 13:40:15 +0900

    tes

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 
 require('../vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+
 date_default_timezone_set("Asia/Tokyo");
 
 $app = new Silex\Application();
@@ -22,6 +24,47 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+
+$app->post('/callback', function (Request $request) use ($app) {
+    $client = new GuzzleHttp\Client();
+
+    $body = json_decode($request->getContent(), true);
+    foreach ($body['result'] as $msg) {
+        if (!preg_match('/(ぬるぽ|ヌルポ|ﾇﾙﾎﾟ|nullpo)/i', $msg['content']['text'])) {
+            continue;
+        }
+
+        $resContent = $msg['content'];
+        $resContent['text'] = 'ｶﾞｯ';
+
+        $requestOptions = [
+            'body' => json_encode([
+                'to' => [$msg['content']['from']],
+                'toChannel' => 1383378250, # Fixed value
+                'eventType' => '138311608800106203', # Fixed value
+                'content' => $resContent,
+            ]),
+            'headers' => [
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'X-Line-ChannelID' => 1499585631,
+                'X-Line-ChannelSecret' => "f27b733e60fc6b4f8459731285adfd89",
+                'X-Line-Trusted-User-With-ACL' => "@dyg2321n",
+            ],
+            'proxy' => [
+                'https' => "http://fixie:hMiDdbvvZHgMotg@velodrome.usefixie.com:80",
+            ],
+        ];
+
+        try {
+            $client->post('https://trialbot-api.line.me/v1/events', $requestOptions);
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+
+    return 'OK';
 });
 
 $app->run();
