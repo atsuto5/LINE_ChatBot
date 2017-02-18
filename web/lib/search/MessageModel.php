19dfad421caec841b92bf7156674c0f852ae8156
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-02-18 14:59:32 +0900

    tes

--- web/lib/search/MessageModel.php
+++ web/lib/search/MessageModel.php
@@ -8,7 +8,7 @@
 require_once ('./lib/LineMessageUtil.php');
 require_once ('./lib/search/DicConstant.php');
 require_once ('./model/LineButtonTemplate.php');
-require_once ('./model/UriTemplateAction.php');
+require_once ('./model/PostBackTemplateAction.php');
 
 class MessageModel {
 
@@ -62,9 +62,9 @@ class MessageModel {
 		    $buttonTemplate->setThumbnailImageUrl($result["image_url"]);
 		    $buttonTemplate->setTitle($result["name"]);
 
-		    $postBackAction = new UriTemplateAction();
+		    $postBackAction = new PostBackTemplateAction();
 		    $postBackAction->setLabel("もっと詳しく");
-            $postBackAction->setUri("https://host=shrouded-badlands-61521.herokuapp.com/uri_action_callback");
+            $postBackAction->setData("data");
 
             $buttonTemplate->addAction($postBackAction);
             $this->messageArray[] = LineMessageUtil::getTemplateMessage("tes",$buttonTemplate);
